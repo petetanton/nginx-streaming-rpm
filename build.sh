399f0c702b026cc6cf766756250a1579d126b6d1
petetanton <pete.tanton@theaudioproject.co.uk>
2016-06-18 12:18:04 +0100

    update to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-rpmdev-wipetree
+#rpmdev-wipetree
 rpmdev-setuptree
 cp ~/jobs/nginx-streaming-build/workspace/nginx-streaming.tar.gz ~/rpmbuild/SOURCES/nginx-streaming.tar.gz
 cp SPECS/nginx-streaming-rpm.spec ~/rpmbuild/SPECS/nginx-streaming.spec
