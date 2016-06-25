7682e85b85d1d26c51d554714a5f9e6ad5901190
petetanton <pete.tanton@theaudioproject.co.uk>
2016-06-25 12:17:43 +0100

    adding basic nginx config moving install script

--- src/scripts/post_install.sh
+++ src/scripts/post_install.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
-mkdir /opt/nginx-build
-cd /opt/nginx-build
-tar -zxvf nginx-streaming.tar.gz
-
-cd nginx-1.9.9
-./configure --user=nginx --group=nginx --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --with-http_ssl_module --with-pcre --add-module=../nginx-rtmp-module
-make
-make install
-useradd -d /etc/nginx/ -s /sbin/nologin nginx
-mv /opt/nginx-build/nginx.sh /etc/init.d/nginx
-chmod +x /etc/init.d/nginx
+echo "Copying config"
+mv /opt/nginx-build/nginx.conf /etc/nginx/nginx.conf
+echo "Finishing up..."
