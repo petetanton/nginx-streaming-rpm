1c92a28adda18634a3546ef373587b8290463d3c
petetanton <pete.tanton@bbc.co.uk>
2017-03-19 16:46:42 +0000

    started work on the jar

--- src/main/java/uk/tanton/streaming/live/http/MissingParameterException.java
+++
@@ -1,7 +0,0 @@
-package uk.tanton.streaming.live.http;
-
-public class MissingParameterException extends Exception {
-    public MissingParameterException(final String message) {
-        super(message);
-    }
-}
