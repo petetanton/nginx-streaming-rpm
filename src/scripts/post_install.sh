fb7a866308d92a66f969324dc656a0e616b6abce
petetanton <pete.tanton@bbc.co.uk>
2017-04-14 13:45:37 +0100

    perform install of java app post_install

--- src/scripts/post_install.sh
+++ src/scripts/post_install.sh
@@ -19,3 +19,12 @@ cp /opt/nginx-build/nginx.conf /etc/nginx/nginx.conf
 echo "Finishing up..."
 
 systemctl enable nginx
+
+inst() {
+  echo "Installing..."
+  install -m 644 -D $1 $2
+}
+
+inst "/opt/streaming-origin/streaming-origin.service" "/etc/systemd/system/streaming-origin.service"
+
+systemctl enable streaming-origin
