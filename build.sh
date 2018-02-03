6e4592b7dfee2075f238ec2a0ec4d71d82eef1a9
petetanton <pete.tanton@bbc.co.uk>
2018-02-03 22:22:58 +0000

    bump version of nginx

--- build.sh
+++ build.sh
@@ -12,13 +12,13 @@ tar xf pcre-8.38.tar.gz
 wget https://github.com/openssl/openssl/archive/OpenSSL_1_0_1t.tar.gz
 tar xf OpenSSL_1_0_1t.tar.gz
 
-wget http://nginx.org/download/nginx-1.9.9.tar.gz
-tar xf nginx-1.9.9.tar.gz
-cd nginx-1.9.9
+wget https://nginx.org/download/nginx-1.12.2.tar.gz
+tar xf nginx-1.12.2.tar.gz
+cd nginx-1.12.2
 ./configure --user=nginx --group=nginx --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --with-http_ssl_module --with-pcre --add-module=../nginx-rtmp-module
 make
 cd ..
-tar cf - nginx-1.9.9/ | gzip > nginx-streaming.tar.gz
+tar cf - nginx-1.12.2/ | gzip > nginx-streaming.tar.gz
 
 cd ..
 
--- src/scripts/post_install.sh
+++ src/scripts/post_install.sh
@@ -4,7 +4,7 @@ mkdir /opt/nginx-build
 cd /opt/nginx-build
 tar -zxvf nginx-streaming.tar.gz
 
-cd nginx-1.9.9
+cd nginx-1.12.2
 ./configure --user=nginx --group=nginx --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --with-http_ssl_module --with-pcre --add-module=../nginx-rtmp-module
 make
 echo "Nginx installing..."
